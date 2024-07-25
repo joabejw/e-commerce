989a99008c2ce14f36e291a96af2d2a48e119cec
joabejw <joabe.18@hotmail.com>
2024-07-25 19:22:24 -0300

    projeto criado

---
+++ src/test/java/com/e_commerce/e_commerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
